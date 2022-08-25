3b2f4af2a029ade676552a222a22ecd3d153a8be
F4LC0NN <thomasruben25@gmail.com>
2022-08-25 14:51:46 +0200

    Build the planets data management

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,13 @@
 const http = require('http');
 const app = require('./app');
 
+const { loadPlanetsData } = require('./models/planets.model')
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
+async function startServer() {
+  await loadPlanetsData();
+  server.listen(PORT, () => console.log(`Server running on ${PORT}!`));
+}
 
-server.listen(PORT, () => console.log(`Server running on ${PORT}!`));
+startServer();
