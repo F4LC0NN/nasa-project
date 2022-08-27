44ccea1af3da1a9c1278f4480d8354a1437b2233
F4LC0NN <thomasruben25@gmail.com>
2022-08-27 11:38:41 +0200

    Test project deployment & Set post launches

--- server/src/app.js
+++ server/src/app.js
@@ -11,9 +11,9 @@ app.use(cors({
   origin: 'http://localhost:3000',
 }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '..', 'public')));
+app.use(express.static(path.join(__dirname, '..', 'build')));
 
-app.use(planetsRouter);
-app.use(launchesRouter);
+app.use('/planets', planetsRouter);
+app.use('/launches', launchesRouter);
 
 module.exports = app;
