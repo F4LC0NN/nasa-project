1db1d009bff0654d23021c91257ea8794fc8599b
F4LC0NN <thomasruben25@gmail.com>
2022-09-02 16:18:10 +0200

    Update server & mongo user

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGO_URL = 'mongodb+srv://nasa-api:i7eTjUJJ4raJLatM@nasa-cluster.yjnkow0.mongodb.net/nasa?retryWrites=true&w=majority';
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once('open', () => {
   console.log('MongoDB connection ready!');
